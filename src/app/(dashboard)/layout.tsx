cfcab4dc3c8cb34b133ad7ce6cbb5d2a31060744
shaho-a <alirezaei.sh94@gmail.com>
2025-11-02 22:33:42 +0330

    fixed: my-account/layout.tsx

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -8,7 +8,7 @@ export default function DashboardLayout({
   return (
     <section className="h-screen flex">
       {/* اینجا بعداً می‌تونی سایدبار داشبورد بذاری */}
-      <aside className="w-64 bg-gray-100 p-4 h-screen">
+      <aside className="w-64 bg-gray-100 hidden md:inline-block p-4 h-screen">
         <h2 className="text-lg text-black pt-5  font-bold">Dashboard Menu</h2>
         {/* بعداً لینک‌ها رو اینجا اضافه می‌کنی */}
       </aside>
